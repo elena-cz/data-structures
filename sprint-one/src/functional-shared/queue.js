50ccd187a290b57494033c4bd5853a76d86b5bbc
elena-cz <elenaczubiak@gmail.com>
2017-10-11 16:06:36 -0700

    done with functional shared method on queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,33 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {
+    first: 0,
+    last: 0
+  };
+  someInstance.storage = {};
+  _.extend(someInstance, queueMethods);
+  return someInstance;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value) {
+  this.storage[this.last] = value;
+  this.last++;
+};
+
+queueMethods.dequeue = function() {
+  if (this.last > this.first) {
+    var removedItem = this.storage[this.first];
+    delete this.storage[this.first];
+    this.first++;
+    return removedItem;  
+  }  
+};
+
+queueMethods.size = function() {
+  return this.last - this.first;
+};
 
 
