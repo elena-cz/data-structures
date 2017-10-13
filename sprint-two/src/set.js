55ebd2e50fc92ee7d85131fb009869e71c0c6733
elena-cz <elenaczubiak@gmail.com>
2017-10-13 10:55:13 -0700

    Added time complexity

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -5,17 +5,19 @@ var Set = function() {
 };
 
 var setPrototype = {};
-
+// time complexity - 0(n)
 setPrototype.add = function(item) {
   if (!this.contains(item)) {
     this._storage.push(item);
   }  
 };
 
+// time complexity - O(n)
 setPrototype.contains = function(item) {
   return (this._storage.indexOf(item) > -1);
 };
 
+//// time complexity -O(n)
 setPrototype.remove = function(item) {
   var index = this._storage.indexOf(item);
   this._storage.splice(index);
